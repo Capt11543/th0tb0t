475c6b0873208ed0c25d35774e22171f93902e26
Capt11543 <41219697+Capt11543@users.noreply.github.com>
2018-11-23 19:07:00 -0500

    Update dueling.py
    -Removed beginnings of file functionality to focus on pure Rock Paper Scissors
    ~Fixed syntax errors
    ~Changed variable names to match with current plan
    +Added temp code for debug purposes

--- dueling.py
+++ dueling.py
@@ -1,21 +1,22 @@
 # Imports
 import async
 import discord
-import main
+from main import client
 
 class Player:  # Class used to represent each player in a duel
-    def __init__(discord_id):  # Constructor
-      this.discord_id = discord_id  # Stores the Discord ID of the player involved
+    def __init__(self, member):  # Constructor
+      self.member = member  # Stores the Discord ID of the player involved
 
-      if not Path(discord_id + ".txt").is_file():  # Checks to see if there's a save file for this person
-        self.save_file = open(discord_id + ".txt",'w')  # If not, create one
-      else:
-        self.save_file = open(discord_id + ".txt",'r')  # If there is, read from it
+      self.choice = None
+
+      print("Player object created successfully with ID " + self.member.id)
 
 class Duel: # Class for doing duels
   def __init__(self, message, player1, player2):  # Constructor
     self.message = message  # Allows me to pass in the message variable from on_message, so there's no errors
-  
     # Objects of the Player class that represent the players
     self.player1 = Player(player1)
     self.player2 = Player(player2)
+    self.phase = 1  # What phase the duel is on.  1 = choosing, 2 = throwing
+
+    print("Duel object created successfully")
