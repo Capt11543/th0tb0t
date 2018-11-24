1bb186657ff5612e7bf6edf6203c144c50fce563
Capt11543 <41219697+Capt11543@users.noreply.github.com>
2018-11-23 19:15:38 -0500

    Update dueling.py
    ~Fixed errors

--- dueling.py
+++ dueling.py
@@ -1,10 +1,9 @@
 # Imports
 import async
 import discord
-from main import client
 
 class Player:  # Class used to represent each player in a duel
-    def __init__(self, member):  # Constructor
+    def __init__(self, client, member):  # Constructor
       self.member = member  # Stores the Discord ID of the player involved
 
       self.choice = None
@@ -12,11 +11,11 @@ class Player:  # Class used to represent each player in a duel
       print("Player object created successfully with ID " + self.member.id)
 
 class Duel: # Class for doing duels
-  def __init__(self, message, player1, player2):  # Constructor
+  def __init__(self, message, client player1, player2):  # Constructor
     self.message = message  # Allows me to pass in the message variable from on_message, so there's no errors
     # Objects of the Player class that represent the players
-    self.player1 = Player(player1)
-    self.player2 = Player(player2)
+    self.player1 = Player(client, player1)
+    self.player2 = Player(client, player2)
     self.phase = 1  # What phase the duel is on.  1 = choosing, 2 = throwing
 
     print("Duel object created successfully")
