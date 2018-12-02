3aacfd57886955715b461aa083360ccf351c1d4f
Capt11543 <41219697+Capt11543@users.noreply.github.com>
2018-12-01 21:45:31 -0500

    Update main.py
    ~Simplified check to just seeing if "owo" is in the message

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def on_message(message):
     await client.delete_message(message)
     print("PRESS F ONLY :reee:")
   
-  if not message.channel.id == "515575113945710593" and ((" owo " in message.content.lower() or " uwu " in message.content.lower()) or (message.content.lower().startswith("owo") or message.content.lower().startswith("uwu") or (message.content.lower().endswith("owo") or message.content.lower().endswith("uwu")))):
+  if not message.channel.id == "515575113945710593" and "owo" in message.content.lower:
     await client.delete_message(message)
     print("OwO quarantined successfully")
     await client.send_message(message.channel, "git ur stinkin anime outta my channel, t h 0 t")
